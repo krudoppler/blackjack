926160331dc76f857b0bfa7f8298cce2726e93df
krudoppler <bkru33@gmail.com>
2024-10-02 19:36:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 // 1. Create two variables, firstCard and secondCard. 
 // Set their values to a random number between 2-11
 firstCard = 10
-secondCard = 12
+secondCard = 10
 
 sum = firstCard + secondCard
-let hasBlackjack = false
-let isAlive = true
+
+let message = ""
+
 if (sum <= 20) {
-    console.log("Would you like to draw another card?")
+    message = "Would you like to draw another card?"
 } else if (sum === 21) {
-    console.log("Woohoo! You have blackjack!")
-    hasBlackjack = true
+    message = "Woohoo! You have blackjack!"
 } else {
-    console.log("You busted!")
-    isAlive = false
+    message = "You busted!"
+   
 }
 
-console.log(isAlive)
+console.log(message)
 
 // 2. Create a variable, sum, and set it to the sum of the two cards
