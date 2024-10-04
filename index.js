174ad898cafc20893322318231b8612473fab742
krudoppler <bkru33@gmail.com>
2024-10-03 21:22:12 -0400

    built out buttons and functions

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
+let messageEl = document.getElementById("message-el")
+// let sumEl = document.getElementById("sum-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+
+
 
 firstCard = 10
 secondCard = 10
-
 sum = firstCard + secondCard
 
-let message = ""
 
-if (sum <= 20) {
-    message = "Would you like to draw another card?"
-} else if (sum === 21) {
-    message = "Woohoo! You have blackjack!"
-} else {
-    message = "You busted!"
-   
+
+function startGame() {
+    sumEl.textContent = "Sum: " + sum
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    if (sum <= 20) {
+        messageEl.innerText = "Would you like to draw another card?"
+    } else if (sum === 21) {
+        messageEl.innerText = "Woohoo! You have blackjack!"
+    } else {
+        messageEl.innerText = "Busted Alert! Busted Alert! Have Fun Staying Poor Jackass!!!"
+    }
 }
 
-console.log(message)
+function newCard() {
+    console.log("Drawing a new card from the deck")
+}
+
+
+
